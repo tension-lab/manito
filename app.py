9a1228297c41120498cc868c44ca6373fc0a4b57
eunbeom <eunbeom.kang@linecorp.com>
2020-04-20 16:17:21 +0900

    본인 안뽑히게

--- app.py
+++ app.py
@@ -3,22 +3,23 @@ import os
 import random
 import time
 
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, render_template
 
 app = Flask(__name__)
 
-member = ['@3unbeom',
-          '@jade_y1004',
-          '@v01dwalker',
-          '@thisisaarroniboyy',
-          '@run.dong8',
-          '@_jiyoung_e',
-          '@ju3unoia',
-          '@rightofsilence',
-          '@__hyun91',
-          '@vely_yoni_official']
+members = ['@3unbeom',
+           '@jade_y1004',
+           '@v01dwalker',
+           '@thisisaarroniboyy',
+           '@run.dong8',
+           '@_jiyoung_e',
+           '@ju3unoia',
+           '@rightofsilence',
+           '@__hyun91',
+           '@vely_yoni_official']
 
-current_list = list(member)
+user_list = list()
+target_list = list(members)
 server_reset_time = str(time.time())
 
 
@@ -28,25 +29,39 @@ def index():
     if server_reset_time == client_reset_time:
         return request.cookies.get('name')
 
-    if len(current_list) == 0:
-        return '다 뽑힘 힝구ㅜ'
+    if len(target_list) == 0:
+        return '다 뽑힘 힝구ㅜㅜ'
 
-    name = random.choice(current_list)
-    current_list.remove(name)
+    if request.method == 'POST':
+        my_name = request.form['myName']
+        choice_list = list(target_list)
+        if my_name in choice_list:
+            choice_list.remove(my_name)
 
-    res = make_response(name)
-    res.set_cookie('name', name)
-    res.set_cookie('reset_time', server_reset_time)
-    return res
+        if len(choice_list) == 0:
+            return '다 뽑힘 힝구ㅜㅜ'
+
+        name = random.choice(choice_list)
+        target_list.remove(name)
+        user_list.append(my_name)
+
+        res = make_response(name)
+        res.set_cookie('name', name)
+        res.set_cookie('reset_time', server_reset_time)
+        return res
+
+    return render_template('index.html', users=user_list, members=members)
 
 
 @app.route("/reset")
 def reset():
-    global current_list, server_reset_time
-    current_list = list(member)
+    global user_list, target_list, server_reset_time
+    user_list = list()
+    target_list = list(members)
     server_reset_time = str(time.time())
-    return f'리셋완료 {current_list}'
+    return f'리셋완료 {target_list}'
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.environ['PORT'])
+    # app.run(host='0.0.0.0', port=os.environ['PORT'])
+    app.run(host='0.0.0.0')
