0807453f6d5d24ddece8bb5b101977c212079d6c
eunbeom <eunbeom.kang@linecorp.com>
2020-04-20 14:25:14 +0900

    heroku deploy

--- app.py
+++ app.py
@@ -40,5 +40,4 @@ def reset():
 
 
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0', port=os.environ['PORT'])
-    app.run(host='0.0.0.0', port='8080')
+    app.run(host='0.0.0.0', port=os.environ['PORT'])
